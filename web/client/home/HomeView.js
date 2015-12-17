1828ea7dad3ab79d63f97f504428ff2d865323f7
Aike J Sommer <aike.sommer@zalando.de>
2015-12-17 11:00:08 +0100

    request specific number of articles

--- web/client/home/HomeView.js
+++ web/client/home/HomeView.js
@@ -15,7 +15,7 @@ class HomeView {
         var element = $('.search.winterschuhe');
         element.click(() => this.search.search('winterschuhe'));
 
-        var articlesRequest = $.getJSON('/articles?search=winterschuhe');
+        var articlesRequest = $.getJSON('/articles?search=winterschuhe&maxResults=15');
         articlesRequest.done(articlesResult => {
             articlesResult.map(article => $(`<li><img src="${article.imageUrls[0]}" /></li>`))
             .forEach(article => article.appendTo(element.find('.results')));
